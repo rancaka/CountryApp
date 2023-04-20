1e472c06b340b2f4cf2bff056d7ea8440e869c63
Adityo Rancaka <adityo.rancaka@paypay-corp.co.jp>
2023-04-21 02:02:18 +0700

    Initial Commit

---
+++ CountryApp/CountryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryAppApp.swift
+//  CountryApp
+//
+//  Created by adityo.rancaka on 2023/04/21.
+//
+
+import SwiftUI
+
+@main
+struct CountryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
